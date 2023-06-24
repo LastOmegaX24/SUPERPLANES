96c7293cbb1f3e88d658d1ac147db9f249dcb732
Last_ <lastomegax24@gmail.com>
2023-06-24 17:35:45 +0900

    X42A Magic update

---
+++ Source/SuperPlane/SuperPlaneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperPlaneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERPLANE_API ASuperPlaneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
